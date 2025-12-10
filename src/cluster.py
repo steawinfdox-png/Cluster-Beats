fb99e15b5ce3fe2820054da842460fc08d042b2a
steawinfdox-png <steawinfdox@gmail.com>
2025-12-10 16:16:15 -0600

    Update cluster.py

--- src/cluster.py
+++ src/cluster.py
@@ -1,1 +1,21 @@
+from sentence_transformers import SentenceTransformer
 
+#generate embeddings
+embedder = SentenceTransformer("all-mpnet-base-v2")
+df["Embedding"] = df["Lyrics"].apply(lambda x: embedder.encode(str(x)).tolist())
+df.to_csv("jvke_song_emotions.csv", index=False)
+df.head
+
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+#compare songs' vectors' cosine similarity and load it into similarity_matrix
+embedding_matrix = np.vstack(df["Embedding"].values)
+similarity_matrix = cosine_similarity(embedding_matrix)
+similarity_matrix[:5, :5]
+
+from sklearn.cluster import KMeans
+#creating clusters by unsupervised learning through K-Means
+kmeans = KMeans(cluster_count, random_state = 42)
+df["Cluster"] = kmeans.fit_predict(embedding_matrix)
+df.to_csv("jvke_song_emotions.csv", index=False)
+df[["Title", "Cluster"]]
